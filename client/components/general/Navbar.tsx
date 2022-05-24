5974bbdc85b50a224ec51d1a0c090e376359c923
Stanley Claudius <stanleyclaudiuss@gmail.com>
2022-05-24 12:49:16 +0700

    Implement responsive style to navbar component

--- client/components/general/Navbar.tsx
+++ client/components/general/Navbar.tsx
@@ -8,20 +8,25 @@ const Navbar = () => {
 
   return (
     <>
-      <HStack justifyContent='space-between' px={32} py={5} boxShadow='0 0 15px rgba(0,0,0,.1)' gap={40}>
-        <Box>
-          <Heading as='h2' size='md' fontWeight='normal'>Discussme</Heading>
+      <Box display='flex' flexDirection={{ base: 'column', lg: 'row' }} justifyContent='space-between' alignItems='center' px={{ base: 10, lg: 32 }} py={5} boxShadow='0 0 15px rgba(0,0,0,.1)' gap={{ base: 8, lg: 40 }}>
+        <Box display='flex' justifyContent='space-between' w={{ base: '100%', lg: 'auto' }}>
+          <Box>
+            <Heading as='h2' size= 'md' fontWeight='normal'>Discussme</Heading>
+          </Box>
+          <Box display={{ base: 'block', lg: 'none' }}>
+            <Button onClick={onOpen} size='sm' colorScheme='blue' fontWeight='normal'>Login</Button>
+          </Box>
         </Box>
-        <Box flex={1}>
+        <Box flex={1} w='100%'>
           <InputGroup bgColor='gray.100' borderRadius={6}>
             <InputLeftElement pointerEvents='none' children={<AiOutlineSearch color='gray.300' />} />
             <Input placeholder='Search for Topics' fontSize='sm' />
           </InputGroup>
         </Box>
-        <Box>
+        <Box display={{ base: 'none', lg: 'block' }}>
           <Button onClick={onOpen} size='sm' colorScheme='blue' fontWeight='normal'>Login</Button>
         </Box>
-      </HStack>
+      </Box>
 
       <AuthModal
         isOpen={isOpen}
