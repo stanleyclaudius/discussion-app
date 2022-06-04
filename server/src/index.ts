84675c84206c08250b447dde54e9ebc08244d206
Stanley Claudius <stanleyclaudiuss@gmail.com>
2022-06-04 18:20:29 +0700

    Remove sameSite and domain key at cookie

--- server/src/index.ts
+++ server/src/index.ts
@@ -55,8 +55,6 @@ const main = async() => {
         maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
         httpOnly: true,
         secure: __prod__,
-        domain: __prod__ ? 'herokuapp.com' : undefined,
-        sameSite: 'lax'
       },
       secret: `${process.env.SESSION_SECRET}`,
       resave: false,
